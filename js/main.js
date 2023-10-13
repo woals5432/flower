6b2b188eadabe1204a7e311f10e1e65ad9fec6f8
woals5432 <woals5432@gmail.com>
2023-10-13 22:17:57 +0900

    0.12

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const menuBtn = document.querySelector('.menuBtn');
 const menu = document.querySelector('.menuBg');
+const nav = document.querySelector('nav');
 
 menuBtn.addEventListener('click', function () {
+  
   if (menu.style.opacity === '0') {
     menu.style.opacity = '1'; // 열기
     menu.style.visibility = "visible";
+    menuBtn.style.background = "black";
+    nav.style.background = "#000";
+    nav.style.color = "white";
+    nav.style.border = "none";
+    document.querySelector('.menuBtn span').style.backgroundImage = "url('../img/noun-flower-1002754-FFFFFF.svg')"
   } else {
     menu.style.opacity = '0'; // 닫기
     menu.style.visibility = "hidden";
+    menuBtn.style.background = "white";
+    nav.style.background = "white";
+    nav.style.color = "#000";
+    nav.style.borderBottom = "1px solid #eee";
+    document.querySelector('.menuBtn span').style.backgroundImage = "url('../img/noun-flower-1002754.svg')"
   }
 })
