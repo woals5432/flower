40954ca852d05fa2b736702267fd8b8af2d9a00d
woals5432 <woals5432@gmail.com>
2023-10-15 13:01:28 +0900

    menubg update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const menu = document.querySelector('.menuBg');
 menuBtn.addEventListener('click', function () {
   if (menu.style.opacity === '0') {
     menu.style.opacity = '1'; // 열기
+    menu.style.visibility = "visible";
   } else {
     menu.style.opacity = '0'; // 닫기
+    menu.style.visibility = "hidden";
   }
 })
