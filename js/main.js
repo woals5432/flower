927144f7cedb864f4d97bb192ddfc9a411dbc5a1
eunae1010 <cea1010@naver.com>
2023-10-17 17:48:17 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// 헤더
-const header = document.querySelector('header');
-window.addEventListener('scroll', () => {
-  if(window.scrollY > 300) {
-    header.classList.add('on');
-  };
-  if(window.scrollY <= 300) {
-    header.classList.remove('on');
-  };
-});
-
 // 메뉴 버튼
 const headerInner = document.querySelector('.header_inner');
 const nav = document.querySelector('.nav_open');
@@ -17,22 +6,10 @@ const navClose = document.querySelector('.nav_closeBtn');
 navBtn.addEventListener('click', () => {
   headerInner.style.color = 'white';
   nav.style.display = 'block';
-
-  window.addEventListener('scroll', () => {
-    if(window.scrollY > 300) {
-      header.classList.remove('on');
-    };
-  });
 });
 navClose.addEventListener('click', () => {
   headerInner.style.color = '';
   nav.style.display = '';
-  
-  window.addEventListener('scroll', () => {
-    if(window.scrollY > 300) {
-      header.classList.add('on');
-    };
-  });
 })
 
 // const menuBtn = document.querySelector('.menuBtn');
