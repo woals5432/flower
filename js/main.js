b27487374fba67a068b51b235fc96cbaa74a6f90
eunae1010 <cea1010@naver.com>
2023-10-17 15:15:42 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// 헤더
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 300) {
+    header.classList.add('on');
+  };
+  if(window.scrollY <= 300) {
+    header.classList.remove('on');
+  };
+});
+
+// 메뉴 버튼
 const headerInner = document.querySelector('.header_inner');
 const nav = document.querySelector('.nav_open');
 const navBtn = document.querySelector('.nav_btn');
@@ -5,8 +17,47 @@ const navClose = document.querySelector('.nav_closeBtn');
 navBtn.addEventListener('click', () => {
   headerInner.style.color = 'white';
   nav.style.display = 'block';
+
+  window.addEventListener('scroll', () => {
+    if(window.scrollY > 300) {
+      header.classList.remove('on');
+    };
+  });
 });
 navClose.addEventListener('click', () => {
   headerInner.style.color = '';
   nav.style.display = '';
+  
+  window.addEventListener('scroll', () => {
+    if(window.scrollY > 300) {
+      header.classList.add('on');
+    };
+  });
 })
+
+// const menuBtn = document.querySelector('.menuBtn');
+// const menu = document.querySelector('.menuBg');
+// const nav = document.querySelector('nav');
+
+// menuBtn.addEventListener('click', function () {
+  
+//   if (menu.style.opacity === '0') {
+//     menu.style.opacity = '1'; // 열기
+//     menu.style.visibility = "visible";
+//     menuBtn.style.background = "black";
+//     nav.style.background = "#000";
+//     nav.style.color = "white";
+//     nav.style.border = "none";
+//     document.querySelector('.menuBtn span').style.background = "url('img/noun-flower-1002754-FFFFFF.svg') no-repeat center center / 100%"
+//     menu.style.visibility = "visible";
+//   } else {
+//     menu.style.opacity = '0'; // 닫기
+//     menu.style.visibility = "hidden";
+//     menuBtn.style.background = "white";
+//     nav.style.background = "white";
+//     nav.style.color = "#000";
+//     nav.style.borderBottom = "1px solid #eee";
+//     document.querySelector('.menuBtn span').style.background = "url('img/noun-flower-1002754.svg') no-repeat center center / 100%"
+//     menu.style.visibility = "hidden";
+//   }
+// })
